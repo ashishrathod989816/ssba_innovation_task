3144a88a6d08b998e2e727c1b6c5449d81d10e1c
ashishrathod <ashish.rathod@eyemagnett.com>
2021-07-12 21:43:48 +0530

    api calling using dio( sample statement added

--- lib/Api_repository/api_service.dart
+++ lib/Api_repository/api_service.dart
@@ -5,6 +5,41 @@ import 'package:ssba_task/Models/resume_model.dart';
 class ApiService {
   static AppDatabase db = AppDatabase();
 
+
+
+//services using dio
+// static Future<CityinfoModel> getStates() async {
+//     var prefs = await SharedPreferences.getInstance();
+//     String? authToken = prefs.getString("authToken");
+
+//     Connection.dio.options.headers = {
+//       // "Content-Type": Connection.contentType,
+//       "x-authorization": Connection.xAuth,
+//       "x-auth-token":authToken
+//     };
+//     // var data = {"mobileNo": mobile, "regOtpId": otpId, "otp": otp};
+//     // FormData formData = FormData.fromMap(data);
+//     // print(formData.fields);
+//     try {
+//       var response = await Connection.dio
+//           .get(Connection.endPoint + "profile/fewer_details");
+//       print(response.data);
+
+//       return StateModel.fromJson(response.data);
+//     } on DioError catch (e) {
+//       print(e);
+//       if (e.type == DioErrorType.connectTimeout)
+//         throw Connection.poorNetwork;
+//       else
+//         throw Connection.somethingWentWrongError;
+//     }
+//   }
+
+
+
+
+
+
   static Future<List<CityinfoModel>> getCities() async {
     try {
       return db.getCityList();
